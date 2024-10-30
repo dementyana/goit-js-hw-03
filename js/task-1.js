16897003eaef90592f6f14280d9462186dc8133b
DementyevaYana <deyananew2000@gmail.com>
2024-10-30 22:10:10 +0100

    all

---
+++ js/task-1.js
@@ -0,0 +1,16 @@
+`use strict`
+
+function slugify(title) {
+    const lowTittle = title.toLowerCase();
+    const words = lowTittle.split(" ");
+
+    return words.join("-");
+}
+
+
+
+console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
